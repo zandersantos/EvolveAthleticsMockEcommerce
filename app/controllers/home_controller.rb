5e388a2f8360217eb01a1762b5b88dcd916e11b7
zandersantos1 <zainzedd@gmail.com>
2025-04-08 13:58:15 -0500

    Implemented home index action and view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @product = Product.order("name DESC").limit(10)
+
+    @categories = Category.includes(:products).limit(10)
   end
 end
