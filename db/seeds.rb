5684c1fe9ccc53fec08fd835cb9867e9c2392b95
zandersantos1 <zainzedd@gmail.com>
2025-04-10 11:31:07 -0500

    Updated seeds.rb to include Produce insertion

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
+
+#Product CREATION
+
+Product.delete_all
+
+
+filenameproduct = Rails.root.join "db/equipment.csv"
+puts "Reading in the file from here #{filenameproduct}"
+
+csv_data = File.read(filenameproduct)
+products = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+products.each do |product|
+
+  # Create product records
+  product_record = Product.create(name: product["name"], description: product["description"], price: product["price"], stockquantity: product["stockquantity"], category_id: product["category_id"])
+
+  if product_record.valid?
+    puts "#{product["name"]} Created"
+  else
+    puts "Unable to create the product name #{product["name"]}"
+    puts "Errors: #{product_record.errors.full_messages.join(", ")}"
+  end
+end
+
+puts "Created #{{Product}.count} Names"
