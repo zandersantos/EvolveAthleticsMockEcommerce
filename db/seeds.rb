60ce42feceb46c27c1e848564eecbb753707f3db
zandersantos1 <zainzedd@gmail.com>
2025-04-10 11:32:46 -0500

    Updated seeds.rb to include Category insertion

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,31 @@
 require "csv"
 
+#Category CREATION
+
+Category.delete_all
+
+
+filenamecategory = Rails.root.join "db/equipment.csv"
+puts "Reading in the file from here #{filenamecategory}"
+
+csv_data = File.read(filenamecategory)
+categories = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+categories.each do |category|
+
+  # Create product records
+  category_record = Product.create(name: category["name"], description: category["description"])
+
+  if category_record.valid?
+    puts "#{category["name"]} Created"
+  else
+    puts "Unable to create the category name #{category["name"]}"
+    puts "Errors: #{category_record.errors.full_messages.join(", ")}"
+  end
+end
+
+puts "Created #{{category}.count} Names"
+
 #Product CREATION
 
 Product.delete_all
