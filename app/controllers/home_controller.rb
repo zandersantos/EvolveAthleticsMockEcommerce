dbc9e0d7e92ce1d5552cb84750d1558b7f7a5054
zandersantos1 <zainzedd@gmail.com>
2025-04-10 16:09:37 -0500

    Updated the category index view header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @product = Product.order("name DESC").limit(10)
+    @product = Product.order("name ASC").limit(10)
 
     @categories = Category.includes(:products).limit(10)
   end
