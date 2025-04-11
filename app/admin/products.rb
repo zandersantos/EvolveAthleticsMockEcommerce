aa39458257896b266148a622d9c6bf6add5987e7
zandersantos1 <zainzedd@gmail.com>
2025-04-11 14:34:54 -0500

    Added a file input for product image

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,7 +1,15 @@
 ActiveAdmin.register Product do
 
-  permit_params :name, :description, :price, :stockquantity, :category_id
+  permit_params :name, :description, :price, :stockquantity, :category_id, :image
 
   remove_filter :image_attachment, :image_blob
 
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
