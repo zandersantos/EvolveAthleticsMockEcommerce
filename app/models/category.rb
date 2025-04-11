e43be83946771c588276f602646cfec08b9ed003
zandersantos1 <zainzedd@gmail.com>
2025-04-11 14:04:45 -0500

    Added ransackable attributes and associations to Category and Product

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,12 @@ class Category < ApplicationRecord
   validates :name, uniqueness: true
 
   has_many :products
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "id_value", "name", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["products"]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
   validates :name, uniqueness: true
   validates :stockquantity, numericality: {only_integer: true}
   validates :price, numericality: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "created_at", "description", "id", "id_value", "name", "price", "stockquantity", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["category"]
+  end
 end
