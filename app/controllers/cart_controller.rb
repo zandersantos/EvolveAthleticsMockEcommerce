de881dbe5876035a2a01c7974dfe3c2b1608625e
zandersantos1 <zainzedd@gmail.com>
2025-04-17 17:48:06 -0500

    Updated the Cart Controller with session-based cart functionality

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,22 @@
 class CartController < ApplicationController
+  before_action :get_product, only: [:create, :destroy]
+
+
   def create
+    unless session[:cart].include?(@product.id)
+      session[:cart] << @product.id
+      flash[:notice] = "#{@product.name} was added to the cart!"
+    end
   end
 
   def destroy
+    if session[:cart].includes?(@product.id)
+      session[:cart].delete(@product.id)
+      flash[:notice] = "#{@product.name} was removed from the cart!"
+  end
+
+  private get_product
+  product_id = params[:id].to_i
+  @product = Product.find(product_id)
   end
 end
