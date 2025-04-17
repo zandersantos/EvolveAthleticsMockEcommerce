a924512dacffa4c944a49523f6f982ba439e27ec
zandersantos1 <zainzedd@gmail.com>
2025-04-17 17:59:36 -0500

    Fixed Spelling error in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cart, only [:create, :destroy]
+  resources :cart, only: [:create, :destroy]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: "home#index"
