8d24c68e548f6493b54a833d8b7931bc4a55693e
zandersantos1 <zainzedd@gmail.com>
2025-04-17 17:35:04 -0500

    Updated the Orders model to include associations and an enumeration of cart or completed for status

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   validates :total, numericality: true
+
+  has_many :order_details
+  has_many :products, through: :order_details
+  enum status: {cart: "cart", completed: "completed"}
 end
