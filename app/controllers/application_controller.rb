d35cd943eacf7195d4ec23514021ae05abcd2708
zandersantos1 <zainzedd@gmail.com>
2025-04-17 17:58:29 -0500

    Replaced Buy now button with Add to cart button in Product partial view. Created session_based cart management in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+
+  def initialize_session
+    session[:cart] ||= []
+  end
+
+  def cart
+    Product.find(session[:cart])
+  end
 end
