0e378bdfbdf6a46559c2ee7d6ddffff477dc9a52
zandersantos1 <zainzedd@gmail.com>
2025-04-21 17:59:35 -0500

    Added ransackable attributes and associations to Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ApplicationRecord
   validates :title, :content, :permalink, presence: true
   validates :permalink, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["content", "created_at", "id", "id_value", "permalink", "title", "updated_at"]
+  end
 end
