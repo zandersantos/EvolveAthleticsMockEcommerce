4ddd95a2f5fd58dded2637ae7aa5f49d675b657b
zandersantos1 <zainzedd@gmail.com>
2025-04-22 16:37:43 -0500

    Updated the Search Controller to include categories

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,6 @@ class SearchController < ApplicationController
     @query = params[:query]
 
     @products = Product.where("name LIKE ?", "%#{query}%")
+    @categories = Category.where("name ILIKE ?", "%#{@query}%")
   end
 end
