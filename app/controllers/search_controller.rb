1bd52afcf500791801e8a1e5ae85b47913fc680b
zandersantos1 <zainzedd@gmail.com>
2025-04-22 16:45:13 -0500

    Fixed the product and category query typo in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def search_all
     @query = params[:query]
 
-    @products = Product.where("name LIKE ?", "%#{query}%")
-    @categories = Category.where("name ILIKE ?", "%#{@query}%")
+    @products = Product.where("name LIKE ?", "%#{@query}%")
+    @categories = Category.where("name LIKE ?", "%#{@query}%")
   end
 end
