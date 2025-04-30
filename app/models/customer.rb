02dc49610f80bacb272d55944b52c7cd3d4a5804
zandersantos1 <zainzedd@gmail.com>
2025-04-29 20:17:11 -0500

    Added basic Devise modules to Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,14 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def email_required?
+    false
+  end
+
+  def email
+    self.email_address
+  end
+
   has_many :orders
 end
