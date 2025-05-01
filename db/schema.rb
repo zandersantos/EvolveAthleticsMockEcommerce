fe199c462bc507dc787ab8052e82f78b19410cdc
zandersantos1 <zainzedd@gmail.com>
2025-05-01 12:40:59 -0500

    Populated the Province table with their associated gst, pst and hst

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_30_011222) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_01_173424) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -138,6 +138,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_30_011222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.decimal "gst"
+    t.decimal "pst"
+    t.decimal "hst"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
