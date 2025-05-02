dee00e1a0b881fe073f5f0897f8d5805a2117cc4
zandersantos1 <zainzedd@gmail.com>
2025-05-02 11:24:24 -0500

    Combined the two search bars and the user is now able to combine their normal query and their category query when searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,30 +1,33 @@
 class SearchController < ApplicationController
+
+  #Category Dropdown search section
+  #This section lets the user do:
+  #1) A normal search with a search bar
+  #2) Search by a category dropdown
+  #3) Search for a prodcut within a category (combines queries)
   def search_all
     @query = params[:query]
-
-    # If the user searches and it matches a category name. Instead display the category search view.
-    # Display all products that is associated with that query
-    matching_category = Category.where("LOWER(name) = ?", @query.downcase).first
-
-    if matching_category
-      @products = Product.where(category_id: matching_category.id)
-      @categories = Category.where(id: matching_category.id)
-      @category_id = matching_category.id
-    else
-      # Search for Products and/or Categories
-      @products = Product.where("name LIKE ?", "%#{@query}%")
-      @categories = Category.where("name LIKE ?", "%#{@query}%")
-    end
-  end
-
-  # Category Dropdown search section
-  def search_by_category
     @category_id = params[:category_id]
 
-    @products = Product.where(category_id: @category_id)
-
-    @categories = Category.where(id: @category_id)
-
-    render :search_all
+    #3
+    #If both the search query and the category dropdown is present
+    if @query.present? && @category_id.present?
+      @products = Product.where(category_id: @category_id).where("name ILIKE ?", "%#{@query}%")
+      @categories = Category.where(id: @category_id)
+    #1
+    #If only the search query is present
+    elsif @query.present?
+      @products = Product.where("name ILIKE ?", "%#{@query}%")
+      @categories = Category.where("name ILIKE ?", "%#{@query}%")
+    #2)
+    #If only the category dropdown is present
+    elsif @category_id.present?
+      @products = Product.where(category_id: @category_id)
+      @categories = Category.where(id: @category_id)
+    #If the user doesnt search and doesnt choose a category, display everything to the user
+    else
+      @products = Product.all
+      @categories = Category.all
+    end
   end
 end
